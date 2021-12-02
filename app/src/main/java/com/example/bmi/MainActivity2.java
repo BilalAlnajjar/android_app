1a27530e25e8e8676d77b0c3bef9ad3981a6ab9a
BilalAlnajjar <bilalalnajjar46@gmail.com>
2021-12-02 04:37:44 +0200

    Other graphical interfaces have been added

--- app/src/main/java/com/example/bmi/MainActivity2.java
+++ app/src/main/java/com/example/bmi/MainActivity2.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main7);
+        setContentView(R.layout.activity_login);
     }
 }
